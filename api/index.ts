7a2ca5d3e32fcfc937c028e589c916a3a57d93aa
I Wayan Dharma Budiyasa <dharmayanggg@gmail.com>
2026-03-02 12:46:15 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -15,7 +15,9 @@ const db = createClient({
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ukmpr-hub.vercel.app");
+  const origin = req.headers.origin;
+  if (origin) res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
